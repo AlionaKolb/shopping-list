d4937f6857de06c2ce6df74f7f6430e2f35a96ae
AlionaKolb <alvibars@mail.ru>
2022-08-20 15:07:49 +0300

    Добавила очистку поля, после Enter

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const sendInput = document.getElementById('input');
 const messagesContainer = document.querySelector('.items');
 
-sendInput.addEventListener('keydown', function(event) {
+sendInput.addEventListener('keyup', function(event) {
     const messageText = sendInput.value;
     const newMessage = document.createElement('div');
     newMessage.classList.add('items');
@@ -13,17 +13,22 @@ sendInput.addEventListener('keydown', function(event) {
 
     if (event.code === 'Enter' && messageText != '') {
         messagesContainer.append(newMessage);
-    }
+        sendInput.value = '';
+    };
+
+
 });
+
+
 
 /* Если кликнуть на элемент списка, он зачеркивается */
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
-messagesContainer.addEventListener("click", (event) => {
-    if (event.target.tagName === "DIV") {
-        const parentDiv = event.target.closest("div");
+messagesContainer.addEventListener("click", (el) => {
+    if (el.target.tagName === "DIV") {
+        const parentDiv = el.target.closest("div");
         parentDiv.classList.toggle("done");
     }
 });
 
-/* Очищать input после добавления нового элемента в список sendInput.value = '';*/
+/* Очищать input после добавления нового элемента в список */
