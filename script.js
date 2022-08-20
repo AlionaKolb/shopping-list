a64cc33fdbf1377883e9b3bf18b7755d0bece1b8
AlionaKolb <alvibars@mail.ru>
2022-08-20 14:27:58 +0300

    Выполнила добавление через Enter, кроме пустых элементов
    Новые элементы добавляются через Enter. Пустые элементы не добавляются

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const sendInput = document.getElementById('input');
+const messagesContainer = document.querySelector('.items');
+
+sendInput.addEventListener('keydown', function(event) {
+    const messageText = sendInput.value;
+    const newMessage = document.createElement('div');
+    newMessage.classList.add('items');
+    newMessage.textContent = messageText;
+
+
+    if (event.code === 'Enter' && messageText != '') {
+        messagesContainer.append(newMessage);
+    };
+
+});
 
 /* Пустые элементы не должны добавляться */
 
@@ -6,4 +21,4 @@
 
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
-/* Очищать input после добавления нового элемента в список */
+/* Очищать input после добавления нового элемента в список sendInput.value = '';*/
