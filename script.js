8d9170c4b203af91713f5789815395f4fbdb4a33
AlionaKolb <alvibars@mail.ru>
2022-08-20 14:49:51 +0300

    Добавила контейнеру items класс done
    Добавила зачеркивание/отмену элементам при клике на них

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+/* Пустые элементы не должны добавляться */
+
 const sendInput = document.getElementById('input');
 const messagesContainer = document.querySelector('.items');
 
@@ -11,14 +13,17 @@ sendInput.addEventListener('keydown', function(event) {
 
     if (event.code === 'Enter' && messageText != '') {
         messagesContainer.append(newMessage);
-    };
-
+    }
 });
 
-/* Пустые элементы не должны добавляться */
-
 /* Если кликнуть на элемент списка, он зачеркивается */
-
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
+
+messagesContainer.addEventListener("click", (event) => {
+    if (event.target.tagName === "DIV") {
+        const parentDiv = event.target.closest("div");
+        parentDiv.classList.toggle("done");
+    }
+});
 
 /* Очищать input после добавления нового элемента в список sendInput.value = '';*/
